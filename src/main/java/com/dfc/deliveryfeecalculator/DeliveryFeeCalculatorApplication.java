10176c2b8c5f17aff565c3756663cf286b20d7a0
stiinasalumets <stiina.salumets@gmail.com>
2024-03-20 09:24:52 +0200

    Initial commit

---
+++ src/main/java/com/dfc/deliveryfeecalculator/DeliveryFeeCalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.dfc.deliveryfeecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DeliveryFeeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryFeeCalculatorApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/dfc/deliveryfeecalculator/DeliveryFeeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfc.deliveryfeecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFeeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
